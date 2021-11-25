f34978491d72207b32d3c25cc1a323789af7c1cb
rubycookinson <57377930+rubycookinson@users.noreply.github.com>
2021-11-24 21:52:38 -0800

    Add files via upload

--- autorun.lua
+++ autorun.lua
@@ -11,29 +11,38 @@ local filesystem = component.filesystem
 
 --=========== NETWORKING ===========--
 
+local function download_and_save(filename, url)
+  local handle, data, chunk = internet.request(url), ""
+  while true do
+    chunk = handle.read(math.huge)
+    if chunk then
+        data = data .. chunk
+    else
+        break
+    end
+  end
+  local f = io.open(filename, "w")
+  f:write(data)
+  f:close()
+end
+
+local function update_all()
+  download_and_save("autorun.lua", "https://raw.githubusercontent.com/rubycookinson/pfos/main/autorun.lua")
+end
+
 m.open(1)
 m.setWakeMessage("update_wakeup")
 
 local function message_handler(_, _, from, port, _, ...)
   local args = {...}
-  
+
   if args[1] == "update_software" then
-    local handle, data, chunk = internet.request("https://raw.githubusercontent.com/rubycookinson/pfos/main/autorun.lua"), ""
-    while true do
-      chunk = handle.read(math.huge)
-      if chunk then
-          data = data .. chunk
-      else
-          break
-      end
-    end
-    local f = io.open("autorun.lua", "w")
-    f:write(data)
-    f:close()
+    update_all()
     computer.shutdown(true)
   end
 end
 
+event.ignore("modem_message", message_handler)
 mh_listener = event.listen("modem_message", message_handler)
 
 --=========== TERMINAL ===========--
@@ -49,13 +58,13 @@ term.setCursor(1, 3)
 while true do
   gpu.setForeground(0x000000)
   gpu.setBackground(0x00DB00)
+
   gpu.fill(1, 1, w, 1, " ")
   gpu.set(4, 1, "PFOS 0.0.1 [devbranch]")
 
   gpu.setForeground(0x00DB00)
   gpu.setBackground(0x000000)
   gpu.fill(1, 2, w, 1, " ")
-
   io.write(" > ")
   gpu.setForeground(0xFFFFFF)
   local input = io.read()
@@ -64,6 +73,11 @@ while true do
   elseif input == "clear" then
     term.clear()
     term.setCursor(1, 3)
+  elseif input == "update" then -- Disable in release
+    update_all()
+    computer.shutdown(true)
+  elseif input == "ac" then
+    os.execute("ac.lua")
   end
 end
 
